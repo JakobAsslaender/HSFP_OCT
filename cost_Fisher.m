96a18b9adedf4d59245d831c6a53818d9c4aeaa2
Jakob Asslaender <jakob.asslaender@nyumc.org>
2017-03-23 12:11:23 -0400

    version used for the radial relaxation paper

--- cost_Fisher.m
+++ cost_Fisher.m
@@ -1,9 +1,12 @@
-function [C, gr] = cost_Fisher(fa, weights, simulator)
+function [C, gr] = cost_Fisher(fa, simulator, weights, correlate)
 
+if nargin < 4 || isempty(correlate)
+    correlate = ones(size(weights));
+end
 
 C = 0;
 if nargout > 1
-    [y,~,dy,~] = simulator(fa);
+    [y,z,dy,~] = simulator(fa);
 else
     y = simulator(fa);
 end
@@ -12,11 +15,11 @@ Nspin  = size(y,3);
 Npulse = length(fa);
 
 for n=1:Nspin
-    I = y(:,:,n)'*y(:,:,n);
+    I = y(:,correlate,n)'*y(:,correlate,n);
     Im1 = inv(I);
     
     % Optimize for the average of all parameters and all spins
-    C = C + sum(diag(Im1) .* weights);
+    C = C + sum(diag(Im1) .* weights(:,n));
     
     % Optimize only for the worst parameter of the worst spin
     %     [Cn, param_idx] = max(diag(Im1));
@@ -29,7 +32,7 @@ for n=1:Nspin
 end
 
 % Normalize the cost
-C = C/(Nspin^2);
+C = C/Nspin;
 
 
 %% Calculate the gradient
@@ -39,12 +42,12 @@ if nargout > 1
     % Optimize for the average of all parameters and all spins
     for k = 1:Npulse
         for n = 1:Nspin
-            for m = size(dy,3):-1:1
-                for l = size(y,2):-1:1
-                    dIda(l,m) = 2 * real(dy(:,k,m,n)'*y(:,l,n));
+            for m = length(correlate):-1:1
+                for l = length(correlate):-1:1
+                    dIda(l,m) = 2 * real(dy(:,k,correlate(m),n)'*y(:,correlate(l),n));
                 end
             end
-            gr(k) = gr(k) - sum(diag(Im1 * dIda * Im1) .* weights);
+            gr(k) = gr(k) - sum(diag(Im1 * dIda * Im1) .* weights(:,n));
         end
     end
     gr = gr /(Nspin^2) ;
@@ -64,5 +67,33 @@ if nargout > 1
 %     end
 %     gr = gr /(Nspin^2);
 end
+
+
+%% Plot result
+% y(:,2,:) = -y(:,2,:) .* repmat(reshape(T1,1,1,[]), [size(y,1) 1 1]);
+% y(:,3,:) =  y(:,3,:) .* repmat(reshape(T2,1,1,[]), [size(y,1) 1 1]);
+
+% figure(1); 
+% subplot(2,2,2); hold off;
+% plot(fa    /pi);
+% xlabel('t (s)'); ylabel('\theta/\pi'); %legend('initial','optimized');
+% 
+% subplot(2,2, [1 3]);
+% hold off; 
+% plot(squeeze(y(:,1,:)),squeeze(z), 'o-');
+% hold all; 
+% plot(sin(0:.01:pi), cos(0:.01:pi), 'black');
+% plot(-sin(0:.01:pi), cos(0:.01:pi), 'black');
+% plot([0 0], [-1 1], 'black');
+% plot([-1 1], [0 0], 'black');
+% % plot( sqrt(T2/T1 * (1/4 - ((0:.01:1) - .5).^2)), 0:.01:1, 'red');
+% % plot(-sqrt(T2/T1 * (1/4 - ((0:.01:1) - .5).^2)), 0:.01:1, 'red');
+% xlabel('y'); ylabel('z'); axis equal;
+% 
+% subplot(2,2,4);
+% hold off;
+% plot(squeeze(y(:,1,:))); 
+% xlabel('t (s)'); ylabel('Fisher Information'); 
+% drawnow;
 
 end
--- radial_relaxation_simulator.m
+++ radial_relaxation_simulator.m
@@ -1,114 +1,151 @@
-function [y,z,dy,dz] = radial_relaxation_simulator(phi, TR, T1, T2, r0)
+function [y,z,dy,dz] = radial_relaxation_simulator(theta, TR, T1, T2, r0)
 
-%% Bloch Simulation
+% theta = col([theta; theta])';
+% TR = TR/2;
+
+%% Dimensions
 Nspin  = length(T1);
-Npulse = length(phi);
+Npulse = length(theta);
+B1     = ones(1, length(T1));
 
-
-D = @(T,T1,T2,phi)[
+%% Define Spin Evolution Matrices
+D = @(T,T1,T2,B1,theta)[
     1,... 
     0;...
-    cos(phi)/(sin(phi)^2*T1/T2+cos(phi)^2) * (1-exp(-T*(sin(phi)^2/T2 + cos(phi)^2/T1))),... 
-    exp(-T*(sin(phi)^2/T2 + cos(phi)^2/T1))];
+    cos(B1*theta)/(sin(B1*theta)^2*T1/T2+cos(B1*theta)^2) * (1-exp(-T*(sin(B1*theta)^2/T2 + cos(B1*theta)^2/T1))),... 
+    exp(-T*(sin(B1*theta)^2/T2 + cos(B1*theta)^2/T1))];
 
-dDdphi = @(T,T1,T2,phi)[
+dDdtheta = @(T,T1,T2,B1,theta)[
     0,... 
     0;...
-    (sin(phi)*(exp(-T*(cos(phi)^2/T1 + sin(phi)^2/T2)) - 1))/(cos(phi)^2 + (T1*sin(phi)^2)/T2) - (cos(phi)*(exp(-T*(cos(phi)^2/T1 + sin(phi)^2/T2)) - 1)*(2*cos(phi)*sin(phi) - (2*T1*cos(phi)*sin(phi))/T2))/(cos(phi)^2 + (T1*sin(phi)^2)/T2)^2 - (T*exp(-T*(cos(phi)^2/T1 + sin(phi)^2/T2))*cos(phi)*((2*cos(phi)*sin(phi))/T1 - (2*cos(phi)*sin(phi))/T2))/(cos(phi)^2 + (T1*sin(phi)^2)/T2),... 
-    T*exp(-T*(cos(phi)^2/T1 + sin(phi)^2/T2))*((2*cos(phi)*sin(phi))/T1 - (2*cos(phi)*sin(phi))/T2)];
+    (B1*sin(B1*theta)*(exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2)) - 1))/(cos(B1*theta)^2 + (T1*sin(B1*theta)^2)/T2) - (cos(B1*theta)*(2*B1*cos(B1*theta)*sin(B1*theta) - (2*B1*T1*cos(B1*theta)*sin(B1*theta))/T2)*(exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2)) - 1))/(cos(B1*theta)^2 + (T1*sin(B1*theta)^2)/T2)^2 - (T*exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*cos(B1*theta)*((2*B1*cos(B1*theta)*sin(B1*theta))/T1 - (2*B1*cos(B1*theta)*sin(B1*theta))/T2))/(cos(B1*theta)^2 + (T1*sin(B1*theta)^2)/T2),...
+    T*exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*((2*B1*cos(B1*theta)*sin(B1*theta))/T1 - (2*B1*cos(B1*theta)*sin(B1*theta))/T2)];
 
-dDdT1 = @(T,T1,T2,phi)[
- 0,...
- 0;...
- (cos(phi)*sin(phi)^2*(exp(-T*(cos(phi)^2/T1 + sin(phi)^2/T2)) - 1))/(T2*(cos(phi)^2 + (T1*sin(phi)^2)/T2)^2) - (T*exp(-T*(cos(phi)^2/T1 + sin(phi)^2/T2))*cos(phi)^3)/(T1^2*(cos(phi)^2 + (T1*sin(phi)^2)/T2)),...
- (T*exp(-T*(cos(phi)^2/T1 + sin(phi)^2/T2))*cos(phi)^2)/T1^2];
+dDdT1 = @(T,T1,T2,B1,theta)[
+    0,...
+    0;...
+    (cos(B1*theta)*sin(B1*theta)^2*(exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2)) - 1))/(T2*(cos(B1*theta)^2 + (T1*sin(B1*theta)^2)/T2)^2) - (T*exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*cos(B1*theta)^3)/(T1^2*(cos(B1*theta)^2 + (T1*sin(B1*theta)^2)/T2)),...
+    (T*exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*cos(B1*theta)^2)/T1^2];
 
-dDdT1dphi = @(T,T1,T2,phi)[
- 0,...
- 0;...
- (exp(-T*(cos(phi)^2/T1 + sin(phi)^2/T2))*sin(phi)*(2*T1^3*T2^2*cos(phi)^4 - T1^4*T2*sin(phi)^4 - 2*T^2*T2^3*cos(phi)^8 - 2*T1^3*T2^2*exp(T*(cos(phi)^2/T1 + sin(phi)^2/T2))*cos(phi)^4 + 3*T1^3*T2^2*cos(phi)^2*sin(phi)^2 + 2*T^2*T1^3*cos(phi)^4*sin(phi)^4 + 2*T*T1^2*T2^2*cos(phi)^6 + 2*T^2*T1*T2^2*cos(phi)^8 + T1^4*T2*exp(T*(cos(phi)^2/T1 + sin(phi)^2/T2))*sin(phi)^4 - 2*T*T1^4*cos(phi)^2*sin(phi)^4 - 2*T1^4*T2*cos(phi)^2*sin(phi)^2 + T*T1*T2^3*cos(phi)^6 + 5*T*T1^3*T2*cos(phi)^2*sin(phi)^4 - 3*T1^3*T2^2*exp(T*(cos(phi)^2/T1 + sin(phi)^2/T2))*cos(phi)^2*sin(phi)^2 + 6*T*T1^2*T2^2*cos(phi)^4*sin(phi)^2 - 4*T^2*T1*T2^2*cos(phi)^6*sin(phi)^2 - 2*T^2*T1^2*T2*cos(phi)^4*sin(phi)^4 + 4*T^2*T1^2*T2*cos(phi)^6*sin(phi)^2 + 2*T1^4*T2*exp(T*(cos(phi)^2/T1 + sin(phi)^2/T2))*cos(phi)^2*sin(phi)^2))/(T1^3*(T2*cos(phi)^2 + T1*sin(phi)^2)^3),...
- -(2*T*exp(-T*(cos(phi)^2/T1 + sin(phi)^2/T2))*cos(phi)*sin(phi)*(T1*T2 + T*T1*cos(phi)^2 - T*T2*cos(phi)^2))/(T1^3*T2)];
+dDdT1dtheta = @(T,T1,T2,B1,theta)[
+    0,...
+    0;...
+    (B1*exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*sin(B1*theta)*(2*T1^3*T2^2*cos(B1*theta)^4 - T1^4*T2*sin(B1*theta)^4 - 2*T^2*T2^3*cos(B1*theta)^8 - 2*T*T1^4*cos(B1*theta)^2*sin(B1*theta)^4 - 2*T1^4*T2*cos(B1*theta)^2*sin(B1*theta)^2 + 3*T1^3*T2^2*cos(B1*theta)^2*sin(B1*theta)^2 + 2*T^2*T1^3*cos(B1*theta)^4*sin(B1*theta)^4 + T*T1*T2^3*cos(B1*theta)^6 + T1^4*T2*exp(T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*sin(B1*theta)^4 + 2*T*T1^2*T2^2*cos(B1*theta)^6 + 2*T^2*T1*T2^2*cos(B1*theta)^8 - 2*T1^3*T2^2*exp(T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*cos(B1*theta)^4 + 5*T*T1^3*T2*cos(B1*theta)^2*sin(B1*theta)^4 + 2*T1^4*T2*exp(T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*cos(B1*theta)^2*sin(B1*theta)^2 + 6*T*T1^2*T2^2*cos(B1*theta)^4*sin(B1*theta)^2 - 4*T^2*T1*T2^2*cos(B1*theta)^6*sin(B1*theta)^2 - 2*T^2*T1^2*T2*cos(B1*theta)^4*sin(B1*theta)^4 + 4*T^2*T1^2*T2*cos(B1*theta)^6*sin(B1*theta)^2 - 3*T1^3*T2^2*exp(T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*cos(B1*theta)^2*sin(B1*theta)^2))/(T1^3*(T2*cos(B1*theta)^2 + T1*sin(B1*theta)^2)^3),...
+    -(2*B1*T*exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*cos(B1*theta)*sin(B1*theta)*(T1*T2 + T*T1*cos(B1*theta)^2 - T*T2*cos(B1*theta)^2))/(T1^3*T2)];
+ 
+dDdT2 = @(T,T1,T2,B1,theta)[
+    0,...
+    0;...
+    -(T*exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*cos(B1*theta)*sin(B1*theta)^2)/(T2^2*(cos(B1*theta)^2 + (T1*sin(B1*theta)^2)/T2)) - (T1*cos(B1*theta)*sin(B1*theta)^2*(exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2)) - 1))/(T2^2*(cos(B1*theta)^2 + (T1*sin(B1*theta)^2)/T2)^2),...
+    (T*exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*sin(B1*theta)^2)/T2^2];
 
-dDdT2 = @(T,T1,T2,phi)[
- 0,...
- 0;...
- - (T*exp(-T*(cos(phi)^2/T1 + sin(phi)^2/T2))*cos(phi)*sin(phi)^2)/(T2^2*(cos(phi)^2 + (T1*sin(phi)^2)/T2)) - (T1*cos(phi)*sin(phi)^2*(exp(-T*(cos(phi)^2/T1 + sin(phi)^2/T2)) - 1))/(T2^2*(cos(phi)^2 + (T1*sin(phi)^2)/T2)^2),...
- (T*exp(-T*(cos(phi)^2/T1 + sin(phi)^2/T2))*sin(phi)^2)/T2^2];
+dDdT2dtheta = @(T,T1,T2,B1,theta)[
+    0,...
+    0;...
+    -(B1*exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*sin(B1*theta)*(2*T1^2*T2^3*cos(B1*theta)^4 - T1^3*T2^2*sin(B1*theta)^4 + T1^3*T2^2*exp(T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*sin(B1*theta)^4 + 3*T1^2*T2^3*cos(B1*theta)^2*sin(B1*theta)^2 - 2*T1^3*T2^2*cos(B1*theta)^2*sin(B1*theta)^2 - 2*T^2*T1^3*cos(B1*theta)^2*sin(B1*theta)^6 + 2*T^2*T2^3*cos(B1*theta)^6*sin(B1*theta)^2 + 2*T*T1*T2^3*cos(B1*theta)^6 - T*T1^3*T2*sin(B1*theta)^6 - 2*T1^2*T2^3*exp(T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*cos(B1*theta)^4 + 3*T*T1*T2^3*cos(B1*theta)^4*sin(B1*theta)^2 - 2*T*T1^3*T2*cos(B1*theta)^2*sin(B1*theta)^4 + 2*T*T1^2*T2^2*cos(B1*theta)^2*sin(B1*theta)^4 + 4*T^2*T1*T2^2*cos(B1*theta)^4*sin(B1*theta)^4 - 2*T^2*T1*T2^2*cos(B1*theta)^6*sin(B1*theta)^2 + 2*T^2*T1^2*T2*cos(B1*theta)^2*sin(B1*theta)^6 - 4*T^2*T1^2*T2*cos(B1*theta)^4*sin(B1*theta)^4 - 3*T1^2*T2^3*exp(T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*cos(B1*theta)^2*sin(B1*theta)^2 + 2*T1^3*T2^2*exp(T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*cos(B1*theta)^2*sin(B1*theta)^2))/(T1*T2^2*(T2*cos(B1*theta)^2 + T1*sin(B1*theta)^2)^3),...
+    (2*B1*T*exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*cos(B1*theta)*sin(B1*theta)*(T1*T2 - T*T1*sin(B1*theta)^2 + T*T2*sin(B1*theta)^2))/(T1*T2^3)];
 
-dDdT2dphi = @(T,T1,T2,phi)[
- 0,...
- 0;...
- -(exp(-T*(cos(phi)^2/T1 + sin(phi)^2/T2))*sin(phi)*(2*T1^2*T2^3*cos(phi)^4 - T1^3*T2^2*sin(phi)^4 - 2*T1^2*T2^3*exp(T*(cos(phi)^2/T1 + sin(phi)^2/T2))*cos(phi)^4 + T1^3*T2^2*exp(T*(cos(phi)^2/T1 + sin(phi)^2/T2))*sin(phi)^4 + 3*T1^2*T2^3*cos(phi)^2*sin(phi)^2 - 2*T1^3*T2^2*cos(phi)^2*sin(phi)^2 - 2*T^2*T1^3*cos(phi)^2*sin(phi)^6 + 2*T^2*T2^3*cos(phi)^6*sin(phi)^2 + 2*T*T1*T2^3*cos(phi)^6 - T*T1^3*T2*sin(phi)^6 + 3*T*T1*T2^3*cos(phi)^4*sin(phi)^2 - 2*T*T1^3*T2*cos(phi)^2*sin(phi)^4 - 3*T1^2*T2^3*exp(T*(cos(phi)^2/T1 + sin(phi)^2/T2))*cos(phi)^2*sin(phi)^2 + 2*T1^3*T2^2*exp(T*(cos(phi)^2/T1 + sin(phi)^2/T2))*cos(phi)^2*sin(phi)^2 + 2*T*T1^2*T2^2*cos(phi)^2*sin(phi)^4 + 4*T^2*T1*T2^2*cos(phi)^4*sin(phi)^4 - 2*T^2*T1*T2^2*cos(phi)^6*sin(phi)^2 + 2*T^2*T1^2*T2*cos(phi)^2*sin(phi)^6 - 4*T^2*T1^2*T2*cos(phi)^4*sin(phi)^4))/(T1*T2^2*(T2*cos(phi)^2 + T1*sin(phi)^2)^3),...
- (2*T*exp(-T*(cos(phi)^2/T1 + sin(phi)^2/T2))*cos(phi)*sin(phi)*(T1*T2 - T*T1*sin(phi)^2 + T*T2*sin(phi)^2))/(T1*T2^3)];
+% dDdB1 = @(T,T1,T2,B1,theta)[
+%     0,... 
+%     0;...
+%     (theta*sin(B1*theta)*(exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2)) - 1))/(cos(B1*theta)^2 + (T1*sin(B1*theta)^2)/T2) - (cos(B1*theta)*(exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2)) - 1)*(2*theta*cos(B1*theta)*sin(B1*theta) - (2*T1*theta*cos(B1*theta)*sin(B1*theta))/T2))/(cos(B1*theta)^2 + (T1*sin(B1*theta)^2)/T2)^2 - (T*exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*cos(B1*theta)*((2*theta*cos(B1*theta)*sin(B1*theta))/T1 - (2*theta*cos(B1*theta)*sin(B1*theta))/T2))/(cos(B1*theta)^2 + (T1*sin(B1*theta)^2)/T2),...
+%     T*exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*((2*theta*cos(B1*theta)*sin(B1*theta))/T1 - (2*theta*cos(B1*theta)*sin(B1*theta))/T2)];
+%  
+% dDdB1dtheta = @(T,T1,T2,B1,theta)[
+%     0,... 
+%     0;...
+%     (T2*sin(B1*theta)*(exp(-(T*(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2))/(T1*T2)) - 1))/(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2) + (T2*cos(B1*theta)*(T1 - T2)*(sin(2*B1*theta) + 2*B1*theta*cos(2*B1*theta))*(exp(-(T*(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2))/(T1*T2)) - 1))/(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2)^2 + (B1*T2*theta*cos(B1*theta)*(exp(-(T*(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2))/(T1*T2)) - 1))/(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2) + (T*exp(-(T*(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2))/(T1*T2))*cos(B1*theta)*(T1 - T2)*(sin(2*B1*theta) + 2*B1*theta*cos(2*B1*theta)))/(T1*(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2)) - (2*B1*T2*theta*cos(B1*theta)*sin(2*B1*theta)^2*(T1 - T2)^2*(exp(-(T*(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2))/(T1*T2)) - 1))/(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2)^3 - (2*B1*T2*theta*sin(B1*theta)*sin(2*B1*theta)*(T1 - T2)*(exp(-(T*(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2))/(T1*T2)) - 1))/(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2)^2 - (2*B1*T*theta*exp(-(T*(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2))/(T1*T2))*sin(B1*theta)*sin(2*B1*theta)*(T1 - T2))/(T1*(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2)) - (2*B1*T*theta*exp(-(T*(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2))/(T1*T2))*cos(B1*theta)*sin(2*B1*theta)^2*(T1 - T2)^2)/(T1*(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2)^2) - (B1*T^2*theta*exp(-(T*(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2))/(T1*T2))*cos(B1*theta)*sin(2*B1*theta)^2*(T1 - T2)^2)/(T1^2*T2*(T2 + T1*sin(B1*theta)^2 - T2*sin(B1*theta)^2)),...
+%     -(2*T*exp(-T*(cos(B1*theta)^2/T1 + sin(B1*theta)^2/T2))*(T1 - T2)*(T1*T2*cos(B1*theta)*sin(B1*theta) + B1*T1*T2*theta*cos(B1*theta)^2 - B1*T1*T2*theta*sin(B1*theta)^2 - 2*B1*T*T1*theta*cos(B1*theta)^2*sin(B1*theta)^2 + 2*B1*T*T2*theta*cos(B1*theta)^2*sin(B1*theta)^2))/(T1^2*T2^2)];
 
 
-
-y  = zeros(Npulse,3,Nspin);        % 'y' component of the magnetization
-z  = zeros(Npulse  ,Nspin);        % 'z' component of the magnetization
+%% Allocate Memory
+r  = zeros(Npulse,3,Nspin);        % radial component of the magnetization and its T1, T2, B1 derivatives
 if nargout > 2
-    dy = zeros(Npulse,Npulse,3,Nspin); % derivative of 'y' relatively to phi(i)
-    dz = zeros(Npulse,Npulse  ,Nspin); % derivative of 'z' relatively to phi(i)
+    dy = zeros(Npulse,Npulse,3,Nspin); % derivative of 'y' relatively to theta(i)
+    dz = zeros(Npulse,Npulse  ,Nspin); % derivative of 'z' relatively to theta(i)
 end
 
+%% Loop over different T1, T2, B1 values, if desired
 for n = Nspin:-1:1
-    r     = [1;r0];
+    ri    = [1;r0];
     drdT1 = [0; 0];
     drdT2 = [0; 0];
+%     drdB1 = [0; 0];
     
     %            changed pulse,affected signal
-    drdT1dphi = zeros(2,Npulse,Npulse);
-    drdT2dphi = zeros(2,Npulse,Npulse);
+    drdT1dtheta = zeros(2,Npulse,Npulse);
+    drdT2dtheta = zeros(2,Npulse,Npulse);
+%     drdB1dtheta = zeros(2,Npulse,Npulse);
     
     if nargout > 2
-        dr      = zeros(Npulse,Npulse);
-        drdphi  = ones(2,Npulse);
-        drdphik = ones(2,Npulse);
-        U       = zeros(Npulse+1,2,2,Npulse);
+        drdtheta = zeros(Npulse,Npulse);
+        dri      = ones(2,Npulse);
+        drk      = ones(2,Npulse);
+        U        = zeros(Npulse+1,2,2,Npulse);
         for k = 1:Npulse+1
             U(k,:,:,k) = eye(2);
         end
     end
     
+    %% Main loop: Calculate spin evolution
     for k = 1:Npulse
         if nargout > 2            
             if k>1
-                drdT1dphi(:,1:k,  k) = D(TR,T1(n),T2(n),phi(k)) * drdT1dphi(:,1:k,k-1);
-                drdT2dphi(:,1:k,  k) = D(TR,T1(n),T2(n),phi(k)) * drdT2dphi(:,1:k,k-1);
-                drdT1dphi(:,1:k-1,k) = drdT1dphi(:,1:k-1,k) + dDdT1(TR,T1(n),T2(n),phi(k)) * drdphi(:,1:k-1);
-                drdT2dphi(:,1:k-1,k) = drdT2dphi(:,1:k-1,k) + dDdT2(TR,T1(n),T2(n),phi(k)) * drdphi(:,1:k-1);
+                drdT1dtheta(:,1:k,  k) = D(TR,T1(n),T2(n),B1(n),theta(k)) * drdT1dtheta(:,1:k,k-1);
+                drdT2dtheta(:,1:k,  k) = D(TR,T1(n),T2(n),B1(n),theta(k)) * drdT2dtheta(:,1:k,k-1);
+%                 drdB1dtheta(:,1:k,  k) = D(TR,T1(n),T2(n),B1(n),theta(k)) * drdB1dtheta(:,1:k,k-1);
+                
+                drdT1dtheta(:,1:k-1,k) = drdT1dtheta(:,1:k-1,k) + dDdT1(TR,T1(n),T2(n),B1(n),theta(k)) * dri(:,1:k-1);
+                drdT2dtheta(:,1:k-1,k) = drdT2dtheta(:,1:k-1,k) + dDdT2(TR,T1(n),T2(n),B1(n),theta(k)) * dri(:,1:k-1);
+%                 drdB1dtheta(:,1:k-1,k) = drdB1dtheta(:,1:k-1,k) + dDdB1(TR,T1(n),T2(n),B1(n),theta(k)) * dri(:,1:k-1);
             end            
-            drdT1dphi(:,k,k) = drdT1dphi(:,k,k) + dDdT1dphi(TR,T1(n),T2(n),phi(k)) * r + dDdphi(TR,T1(n),T2(n),phi(k)) * drdT1;
-            drdT2dphi(:,k,k) = drdT2dphi(:,k,k) + dDdT2dphi(TR,T1(n),T2(n),phi(k)) * r + dDdphi(TR,T1(n),T2(n),phi(k)) * drdT2;
+            drdT1dtheta(:,k,k) = drdT1dtheta(:,k,k) + dDdT1dtheta(TR,T1(n),T2(n),B1(n),theta(k)) * ri + dDdtheta(TR,T1(n),T2(n),B1(n),theta(k)) * drdT1;
+            drdT2dtheta(:,k,k) = drdT2dtheta(:,k,k) + dDdT2dtheta(TR,T1(n),T2(n),B1(n),theta(k)) * ri + dDdtheta(TR,T1(n),T2(n),B1(n),theta(k)) * drdT2;
+%             drdB1dtheta(:,k,k) = drdB1dtheta(:,k,k) + dDdB1dtheta(TR,T1(n),T2(n),B1(n),theta(k)) * ri + dDdtheta(TR,T1(n),T2(n),B1(n),theta(k)) * drdB1;
             
-            drdphi (:,1:k-1) =      D(TR,T1(n),T2(n),phi(k)) * drdphi(:,1:k-1);
-            drdphi (:,  k)   = dDdphi(TR,T1(n),T2(n),phi(k)) * r;
-            drdphik(:,  k)   = drdphi(:,k);
+            dri(:,1:k-1) =        D(TR,T1(n),T2(n),B1(n),theta(k)) * dri(:,1:k-1);
+            dri(:,  k)   = dDdtheta(TR,T1(n),T2(n),B1(n),theta(k)) * ri;
+            drk(:,  k)   = dri(:,k);
             
-            U(k+1,:,:,1:k) = reshape(D(TR,T1(n),T2(n),phi(k)) * reshape(U(k,:,:,1:k), [2 2*k]), [1 2 2 k]);
+            U(k+1,:,:,1:k) = reshape(D(TR,T1(n),T2(n),B1(n),theta(k)) * reshape(U(k,:,:,1:k), [2 2*k]), [1 2 2 k]);
         end
         
-        drdT1 = dDdT1(TR,T1(n),T2(n),phi(k)) * r + D(TR,T1(n),T2(n),phi(k)) * drdT1;
-        drdT2 = dDdT2(TR,T1(n),T2(n),phi(k)) * r + D(TR,T1(n),T2(n),phi(k)) * drdT2;
-        r     =     D(TR,T1(n),T2(n),phi(k)) * r;
+        drdT1 = dDdT1(TR,T1(n),T2(n),B1(n),theta(k)) * ri + D(TR,T1(n),T2(n),B1(n),theta(k)) * drdT1;
+        drdT2 = dDdT2(TR,T1(n),T2(n),B1(n),theta(k)) * ri + D(TR,T1(n),T2(n),B1(n),theta(k)) * drdT2;
+%         drdB1 = dDdB1(TR,T1(n),T2(n),B1(n),theta(k)) * ri + D(TR,T1(n),T2(n),B1(n),theta(k)) * drdB1;
+        ri    =     D(TR,T1(n),T2(n),B1(n),theta(k)) * ri;
         
-        y(k,1,n) = r    (2) .* sin(phi(k));
-        y(k,2,n) = drdT1(2) .* sin(phi(k));
-        y(k,3,n) = drdT2(2) .* sin(phi(k));
-        z(k,  n) = r    (2) .* cos(phi(k));
+     
+        r(k,1,n) = ri   (2);
+        r(k,2,n) = drdT1(2);
+        r(k,3,n) = drdT2(2);
+%         r(k,4,n) = drdB1(2);
     end
+    
 
-    %% If nargout > 2, the derivative of y relatively to u is also computed
+    %% If nargout > 2, loop once more to calculate all dy/dtheta_k 
     if nargout > 2
         for k = 1:Npulse % index of the changed pulse
-            dM = reshape(U(k+1:Npulse+1,:,:,k+1), [2*(Npulse-k+1) 2]) * drdphik(:,k);
-            dr(k:Npulse,k) = reshape(dM, [(Npulse-k+1) 2])*[0; 1];
+            dM = reshape(U(k+1:Npulse+1,:,:,k+1), [2*(Npulse-k+1) 2]) * drk(:,k);
+            drdtheta(k:Npulse,k) = reshape(dM, [(Npulse-k+1) 2])*[0; 1];
         end
-        dz(:,:,  n) = dr(:,:)                     .* repmat(cos(phi).' , [1 Npulse]) - diag(z(:,  n) .* tan(phi).');
-        dy(:,:,1,n) = dr(:,:)                     .* repmat(sin(phi).' , [1 Npulse]) + diag(y(:,1,n) .* cot(phi).');
-        dy(:,:,2,n) = squeeze(drdT1dphi(2,:,:)).' .* repmat(sin(phi).' , [1 Npulse]) + diag(y(:,2,n) .* cot(phi).');
-        dy(:,:,3,n) = squeeze(drdT2dphi(2,:,:)).' .* repmat(sin(phi).' , [1 Npulse]) + diag(y(:,3,n) .* cot(phi).');
+        dz(:,:,  n) =         drdtheta              .* repmat(cos(B1(n)*theta.'), [1 Npulse]) - diag(r(:,1,n) .* B1(n) .* sin(B1(n)*theta.'));
+        dy(:,:,1,n) =         drdtheta              .* repmat(sin(B1(n)*theta.'), [1 Npulse]) + diag(r(:,1,n) .* B1(n) .* cos(B1(n)*theta.'));
+        dy(:,:,2,n) = squeeze(drdT1dtheta(2,:,:)).' .* repmat(sin(B1(n)*theta.'), [1 Npulse]) + diag(r(:,2,n) .* B1(n) .* cos(B1(n)*theta.'));
+        dy(:,:,3,n) = squeeze(drdT2dtheta(2,:,:)).' .* repmat(sin(B1(n)*theta.'), [1 Npulse]) + diag(r(:,3,n) .* B1(n) .* cos(B1(n)*theta.'));
+%         dy(:,:,4,n) = squeeze(drdB1dtheta(2,:,:)).' .* repmat(sin(B1(n)*theta.'), [1 Npulse]) + diag(r(:,4,n) .* B1(n) .* cos(B1(n)*theta.')) ...
+%                     +         drdtheta   .* repmat(theta.' .* cos(B1(n)*theta.'), [1 Npulse]) + diag(r(:,1,n) .*         (cos(B1(n)*theta.') - B1(n) .* theta.' .* sin(B1(n)*theta.')));
     end
 end
 
+z = squeeze(r(:,1,:)) .* repmat(cos(B1(n)*theta.'), [1   Nspin]);
+y =         r         .* repmat(sin(B1(n)*theta.'), [1 3 Nspin]);
+
+% y =  y(1:2:end,:);
+% z =  z(1:2:end,:);
+% if nargout > 2
+%     dy = dy(1:2:end,1:2:end,:);
+%     dz = dz(1:2:end,1:2:end,:);
+% end
 
 end
