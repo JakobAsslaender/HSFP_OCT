defbef9f154e9c055cad970f123fb92763eb3f74
Cem Gultekin <cg3306@nyu.edu>
2020-12-15 22:17:44 +0000

    free_precession/rf_pulse separation

--- simulate_MT_ODE.m
+++ simulate_MT_ODE.m
@@ -24,6 +24,15 @@ else
     r0 = [m0s-1; m0s; 1];
 end
 
+
+
+N=floor(Tmax/TR)-1;
+fpgrid=TR/2+TR*(0:N).';
+rfbeg=fpgrid(2:end)-TR/2-TRF/2;
+rfend=fpgrid(2:end)-TR/2+TRF/2;
+span=[0;sort([rfbeg;rfend]);Tmax];
+span=cat(1,span(1:end-1).',span(2:end).');
+
 f = @(t,r) radial_MT_ODE(t,r, xfun, TR, Tmax, m0s, T1, T2f, R, T2s);
 % f = @(t,r) radial_MT_ODE_XTrans(t,r, xfun, TR, Tmax, m0s, T1, T2f, R, T2s);
 
@@ -32,8 +41,19 @@ options = odeset('RelTol', 1e-9, 'AbsTol', 1e-12);
 % options = odeset('RelTol', 1e5, 'AbsTol', 1e5, 'MaxStep', 4.5e-4, 'InitialStep', 4.5e-4);
 
 for ir = 1:3
-    [~, b] = ode45(f,t,r0,options);
-    r0 = b(end,:);
+    for j=1:size(span,2)
+        switch mod(j,2)
+                case 1
+                    f = @(t,r) radial_MT_ODE(t,r, xfun, 'free_precession', Tmax, m0s, T1, T2f, R, T2s);
+                case 0
+                    f = @(t,r) radial_MT_ODE(t,r, xfun, 'rf_pulse', Tmax, m0s, T1, T2f, R, T2s);
+        end
+        sol = ode45(f,[span(1,j),span(2,j)],r0,options);
+        r0 = deval(sol,span(2,j));
+        locs=find( t<=span(2,j) & t>=span(1,j));
+        b(locs,:)=deval(sol,t(locs));
+    end
+    
     r0(1:3:end) = -r0(1:3:end); % anti periodic boundary conditions for the free pool
 %     r0(2:3:end) =  r0(2:3:end) * (1 - pi^2 * T2s/1e3/max(x(2,:))); % periodic boundary conditions for the semi-solid pool, attenuated by the inversion pulse
     r0(2:3:end) =  r0(2:3:end) * exp(- pi^2 * T2s/max(x(2,:))); % periodic boundary conditions for the semi-solid pool, attenuated by the inversion pulse
